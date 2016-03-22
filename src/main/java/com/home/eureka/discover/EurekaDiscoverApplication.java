803d1e379577ccc8892a25b3dbdc0ce73afc3f88
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-22 14:18:51 +0100

    Initial commit

---
+++ src/main/java/com/home/eureka/discover/EurekaDiscoverApplication.java
@@ -0,0 +1,12 @@
+package com.home.eureka.discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoverApplication.class, args);
+	}
+}
