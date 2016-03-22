ba09c81d2bece91f88c8cffb9ad3f959976b16a7
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-22 14:30:12 +0100

    Add important configuration

--- src/main/java/com/home/eureka/discover/EurekaDiscoverApplication.java
+++ src/main/java/com/home/eureka/discover/EurekaDiscoverApplication.java
@@ -2,10 +2,11 @@ package com.home.eureka.discover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaDiscoverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaDiscoverApplication.class, args);
 	}
