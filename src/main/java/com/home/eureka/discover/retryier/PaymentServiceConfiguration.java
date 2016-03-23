6f4a190dd2a63ff63351826a0458b611866c67fb
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-23 13:20:44 +0100

    Add error decoder

--- src/main/java/com/home/eureka/discover/retryier/PaymentServiceConfiguration.java
+++ src/main/java/com/home/eureka/discover/retryier/PaymentServiceConfiguration.java
@@ -2,6 +2,8 @@ package com.home.eureka.discover.retryier;
 
 import com.home.eureka.discover.ExcludedFromScan;
 import com.home.eureka.discover.OrderResponse;
+import feign.RetryableException;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,17 @@ public class PaymentServiceConfiguration extends FeignClientsConfiguration {
     @Bean
     public MyOrderResponse myOrderService() {
         return new MyOrderResponse();
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return (methodKey, response) -> {
+            if (response.status() == 503) {
+                return new RetryableException("retry", null);
+            }
+
+            return new ErrorDecoder.Default().decode(methodKey, response);
+        };
     }
 
     public class MyOrderResponse implements OrderService {
