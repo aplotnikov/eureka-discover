255a6d2627034f3d5a7abd698d7fcc1efc17f17f
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-23 15:10:08 +0100

    Add using properties from configuration server

--- src/main/java/com/home/eureka/discover/retryier/OrderService.java
+++ src/main/java/com/home/eureka/discover/retryier/OrderService.java
@@ -1,12 +1,11 @@
 package com.home.eureka.discover.retryier;
 
 import com.home.eureka.discover.OrderResponse;
-import feign.codec.ErrorDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "order", configuration = PaymentServiceConfiguration.class, fallback = PaymentServiceConfiguration.MyOrderResponse.class)
+@FeignClient(value = "${order.service.name}", configuration = PaymentServiceConfiguration.class, fallback = PaymentServiceConfiguration.MyOrderResponse.class)
 public interface OrderService {
     @RequestMapping(value = "/orders", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     OrderResponse orderMovie(OrderResponse orderResponse);
